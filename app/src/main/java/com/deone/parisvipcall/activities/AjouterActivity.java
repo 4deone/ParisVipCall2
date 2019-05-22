a9805adc5b9bf3e7db696e46aad9065bbd6d7fa4
Patrick NKOA <ndjanda@gmail.com>
2019-05-22 14:47:09 +0100

    Creation des activités voir et ajouter informations

---
+++ app/src/main/java/com/deone/parisvipcall/activities/AjouterActivity.java
@@ -0,0 +1,15 @@
+package com.deone.parisvipcall.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.deone.parisvipcall.R;
+
+public class AjouterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter);
+    }
+}
--- app/src/main/java/com/deone/parisvipcall/activities/MainActivity.java
+++ app/src/main/java/com/deone/parisvipcall/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.deone.parisvipcall;
+package com.deone.parisvipcall.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.deone.parisvipcall.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 

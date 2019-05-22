f547ffd05386e6f330bf89ab8fc5a3ef5b2916f9
Patrick NKOA <ndjanda@gmail.com>
2019-05-22 15:55:23 +0100

    Creation de la classe Keys

---
+++ app/src/main/java/com/deone/parisvipcall/util/Keys.java
@@ -0,0 +1,11 @@
+package com.deone.parisvipcall.util;
+
+public class Keys {
+    public static final String KEY_CONTACTS = "Authentification";
+    public static final String KEY_LOGIN = "login";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_TYPE_CHOOSE = "typeChoose";
+    public static final String KEY_PRIVATE_MODE = "privateMode";
+    public static final String KEY_DATE = "date";
+    public static final String KEY_COMMENTAIRE = "commentaire";
+}

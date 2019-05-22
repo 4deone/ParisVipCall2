d0daf151efadb97ca3d2f9deba5ca8c19e4d5d75
Patrick NKOA <ndjanda@gmail.com>
2019-05-22 14:36:43 +0100

    Prise ne compte dans le code de notre MainActivity des boutons et gestion du click sur l'un d'entre eux

--- app/src/main/java/com/deone/parisvipcall/MainActivity.java
+++ app/src/main/java/com/deone/parisvipcall/MainActivity.java
@@ -2,12 +2,30 @@ package com.deone.parisvipcall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button btnAddInfo;
+    private Button btnVoirInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnAddInfo = (Button)findViewById(R.id.btnAddInfo);
+        this.btnVoirInfo = (Button)findViewById(R.id.btnVoirInfo);
+        this.btnAddInfo.setOnClickListener(this);
+        this.btnVoirInfo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnAddInfo){
+
+        }else if(v == btnVoirInfo){
+
+        }
     }
 }

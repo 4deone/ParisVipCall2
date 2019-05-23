a8a99aa3db8589847043fddfc19fc3ddd9e2d5ce
Patrick NKOA <ndjanda@gmail.com>
2019-05-23 17:43:05 +0100

    Mise en page de la vue de connexion des utilisateurs

--- app/src/main/java/com/deone/parisvipcall/activities/HomeActivity.java
+++ app/src/main/java/com/deone/parisvipcall/activities/HomeActivity.java
@@ -8,31 +8,31 @@ import android.widget.Button;
 
 import com.deone.parisvipcall.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnAddInfo;
     private Button btnVoirInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         this.initConfiguration();
     }
 
     private void initConfiguration() {
-        /*this.btnAddInfo = (Button)findViewById(R.id.btnAddInfo);
+        this.btnAddInfo = (Button)findViewById(R.id.btnAddInfo);
         this.btnVoirInfo = (Button)findViewById(R.id.btnVoirInfo);
         this.btnAddInfo.setOnClickListener(this);
-        this.btnVoirInfo.setOnClickListener(this);*/
+        this.btnVoirInfo.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if(v == btnAddInfo){
-            //finish();
-            //startActivity(new Intent(MainActivity.this, AjouterActivity.class));
+            finish();
+            startActivity(new Intent(HomeActivity.this, AjouterActivity.class));
         }else if(v == btnVoirInfo){
-            //startActivity(new Intent(MainActivity.this, VoirActivity.class));
+            startActivity(new Intent(HomeActivity.this, VoirActivity.class));
         }
     }
 }

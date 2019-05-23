79d177b5abfc7d061f5f4cf1837f19ba976a2d3f
Patrick NKOA <ndjanda@gmail.com>
2019-05-23 22:24:45 +0100

    Mise en place d'un menu drawer

--- app/src/main/java/com/deone/parisvipcall/activities/HomeActivity.java
+++ app/src/main/java/com/deone/parisvipcall/activities/HomeActivity.java
@@ -1,38 +1,72 @@
 package com.deone.parisvipcall.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 import com.deone.parisvipcall.R;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private Button btnAddInfo;
-    private Button btnVoirInfo;
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        this.initConfiguration();
-    }
 
-    private void initConfiguration() {
-        this.btnAddInfo = (Button)findViewById(R.id.btnAddInfo);
-        this.btnVoirInfo = (Button)findViewById(R.id.btnVoirInfo);
-        this.btnAddInfo.setOnClickListener(this);
-        this.btnVoirInfo.setOnClickListener(this);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar
+        ,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
     }
 
     @Override
-    public void onClick(View v) {
-        if(v == btnAddInfo){
-            finish();
-            startActivity(new Intent(HomeActivity.this, AjouterActivity.class));
-        }else if(v == btnVoirInfo){
-            startActivity(new Intent(HomeActivity.this, VoirActivity.class));
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(Gravity.START)){
+            drawerLayout.closeDrawer(Gravity.START);
+        }else {
+            super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_profile:
+                break;
+            case R.id.nav_chat:
+                break;
+            case R.id.nav_add_password:
+                finish();
+                startActivity(new Intent(HomeActivity.this, AjouterActivity.class));
+                break;
+            case R.id.nav_see_password:
+                startActivity(new Intent(HomeActivity.this, VoirActivity.class));
+                break;
+            case R.id.nav_see_cv:
+                break;
+            case R.id.nav_add_cv:
+                break;
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_deconnexion:
+                break;
+            default:
+        }
+        return true;
     }
 }
